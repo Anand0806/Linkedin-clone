2b13bf8a6a4af517e7970f60e65a3222d4dc3152
anand <anandraopawar08@gmail.com>
2021-01-01 21:04:59 +0530

    Header: Header left styling
    header left : added icon and search bar along with styles

--- linkedin-clone/src/Header.js
+++ linkedin-clone/src/Header.js
@@ -1,16 +1,27 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
+import SearchIcon from '@material-ui/icons/Search';
+import './Header.css'
 
 const Header = props => {
     return (
         <div className="header">
-            <h1>This is header</h1>
+            <div className="header__left">
+                <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt=""/>
+                <div className="header__search">
+                    <SearchIcon/>
+                    <input type="text"/>
+                </div>
+            </div>
+            <div className="header__right">
+
+            </div>
         </div>
     )
 }
 
-Header.propTypes = {
+// Header.propTypes = {
 
-}
+// }
 
 export default Header
