727d907a39a40f01a92b77a38463c4462f8956d0
anand <anandraopawar08@gmail.com>
2021-01-12 20:29:24 +0530

    Feed component build completed synchronously adding Redux to project

--- linkedin-clone/src/Feed.js
+++ linkedin-clone/src/Feed.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Feed.css';
 import CreateIcon from '@material-ui/icons/Create'
 import InputOption from './InputOption';
@@ -7,12 +7,33 @@ import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 import EventNoteIcon from '@material-ui/icons/Event';
 import CalenderViewDayIcon from '@material-ui/icons/CalendarViewDay';
 import Post from './Post';
+import { db } from './firebase';
+import firebase from 'firebase';
 
 const Feed = () => {
     const [posts, setPosts] = useState([]);
+    const [input, setInput] = useState("")
+
+    useEffect(() => {
+        db.collection("posts").orderBy('timestamp', 'desc').onSnapshot((snapshot) => 
+            setPosts(
+                snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    data: doc.data(),
+                }))
+            ))
+    }, [])
 
     const sendPost = e => {
         e.preventDefault();
+        db.collection("posts").add({
+            name: "Anand Rao",
+            description: "This is Anand",
+            message: input,
+            photoUrl: "",
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        setInput("")
     }
 
     return (
@@ -21,7 +42,7 @@ const Feed = () => {
                 <div className="feed__input">
                     <CreateIcon/>
                     <form>
-                        <input type="text"/>
+                        <input value={input} onChange={e => setInput(e.target.value)} type="text"/>
                         <button onClick={sendPost} type="submit">Send</button>
                     </form>
                 </div>
@@ -32,11 +53,17 @@ const Feed = () => {
                     <InputOption Icon={CalenderViewDayIcon} title="Write article" color="#7FC15E"/>
                 </div>
             </div>
-            {posts.map((post => (
-                <Post/>
-            )))}
+            {posts.map(({id, data: {name, description, message, photoUrl}}) => (
+                <Post
+                    key={id}
+                    name={name}
+                    description={description}
+                    message={message}
+                    photoUrl={photoUrl}
+                />
+            ))}
 
-            <Post name="Anand Rao" description="This is a test" message="Wow!! this works"/>
+            {/* <Post name="Anand Rao" description="This is a test" message="Wow!! this works"/> */}
         </div>
     )
 }
